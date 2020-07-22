1a214322c820c422184920586760ceec80d1b5c9
Ruh <ruhphorte@gmail.com>
2020-07-22 16:23:26 -0700

    Importing project

---
+++ Ruhndomizer v2/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Ruhndomizer_v2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.checkBox = new System.Windows.Forms.CheckBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // checkBox
+            // 
+            this.checkBox.AutoSize = true;
+            this.checkBox.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox.ForeColor = System.Drawing.Color.White;
+            this.checkBox.Location = new System.Drawing.Point(11, 4);
+            this.checkBox.Name = "checkBox";
+            this.checkBox.Size = new System.Drawing.Size(139, 29);
+            this.checkBox.TabIndex = 0;
+            this.checkBox.Text = "Every Cell";
+            this.checkBox.UseVisualStyleBackColor = false;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.BackColor = System.Drawing.Color.Black;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.LinkColor = System.Drawing.Color.MediumPurple;
+            this.linkLabel1.Location = new System.Drawing.Point(210, 365);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(180, 20);
+            this.linkLabel1.TabIndex = 1;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "twitter.com/ruhphorte";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.ForeColor = System.Drawing.Color.White;
+            this.checkBox1.Location = new System.Drawing.Point(11, 39);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(171, 29);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Every 3 Cells";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox2.ForeColor = System.Drawing.Color.White;
+            this.checkBox2.Location = new System.Drawing.Point(12, 74);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(71, 29);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "Eco";
+            this.checkBox2.UseVisualStyleBackColor = false;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox3.ForeColor = System.Drawing.Color.White;
+            this.checkBox3.Location = new System.Drawing.Point(11, 109);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(150, 29);
+            this.checkBox3.TabIndex = 4;
+            this.checkBox3.Text = "One Hit Kill";
+            this.checkBox3.UseVisualStyleBackColor = false;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.BackColor = System.Drawing.Color.Black;
+            this.linkLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel2.LinkColor = System.Drawing.Color.MediumPurple;
+            this.linkLabel2.Location = new System.Drawing.Point(204, 4);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(186, 20);
+            this.linkLabel2.TabIndex = 5;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Jak Speedrun Discord";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(396, 394);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.checkBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Ruhndomizer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.CheckBox checkBox;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+    }
+}
+
---
+++ Ruhndomizer v2/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Memory;
+// Randomizer created by Riley Fort and Taylor Berukoff
+namespace Ruhndomizer_v2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public Mem m = new Mem();
+
+        // Array for Address 207CB984
+        string[] spawn = { "0x04 0x73", "0x04 0x53", "0xF4 0x0A", "0x74 0x54", "0x94 0xF2", "0x6C 0x54", "0xAC 0x22", "0x9C 0xF2", "0xF4 0xE2", "0x84 0x62", "0x64 0x54", "0xC4 0x92", "0x5C 0x92", "0x84 0x2A", "0x84 0x52", "0xBC 0x52", "0x04 0x53", "0xEC 0xF2", "0xBC 0x52" , "0xBC 0x52", "0xBC 0x52", "0xBC 0x52", "0x6C 0x54", "0x9C 0xF2", "0x9C 0xF2", "0x9C 0xF2", "0x9C 0xF2", "0x9C 0xF2", "0xF4 0xE2", "0xF4 0xE2", "0xF4 0xE2", "0xF4 0xE2", "0xF4 0xE2", "0xF4 0xE2", "0x84 0x62", "0x84 0x62", "0x64 0x54", "0xC4 0x92", "0xEC 0x22", "0xEC 0x22", "0xC4 0x92", "0x74 0x92", "0x84 0x2A", "0x84 0x2A", "0x84 0x2A", "0x84 0x2A", "0x84 0x2A", "0x84 0x2A", "0x84 0x2A", "0x84 0x2A", "0x5C 0x92", "0x5C 0x92", "0x5C 0x92"};
+        
+        // Array for Address 20642D6C
+        string[] checkpoint = { "0xA4 0xB8", "0x04 0xA8", "0xB4 0xC4", "0x34 0xBF", "0x14 0xB7", "0x04 0xA6", "0x44 0x92", "0x44 0xA3", "0x44 0x9C", "0xB4 0x90", "0xF4 0x8D", "0x44 0x84", "0xE4 0x7D", "0x14 0x8B", "0xF4 0x79", "0x34 0xB2", "0x64 0xA7", "0x24 0xB5", "0xF4 0xAD" , "0x14 0xB0", "0xB4 0xA9", "0xD4 0xAB", "0xC4 0xA4", "0xB4 0xA2", "0x24 0xA2", "0x84 0xA1", "0xF4 0x9F", "0x54 0x9F", "0xE4 0x9A", "0x84 0x99", "0x24 0x98", "0xC4 0x96", "0x64 0x95", "0x04 0x94", "0x14 0x90", "0x74 0x8F", "0xB4 0x8C", "0x04 0x83", "0xE4 0x7F", "0x44 0x7F", "0xA4 0x83", "0x64 0x81", "0x94 0x88", "0xD4 0x89", "0x14 0x86", "0xF4 0x87", "0xB4 0x86", "0x54 0x87", "0x74 0x8A", "0x34 0x89", "0x44 0x7D", "0xA4 0x7C", "0x04 0x7C"};
+
+        string[] eco = {"1", "2", "3", "4" }; // Integers need to be set to string.
+
+        string[] health = { "1" }; // Integers need to be set to string.
+
+        float cellCount = 0; // Sets cell count to 0.
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (true)
+            {
+                int ID = m.getProcIDFromName("pcsx2");
+
+                bool openProc = false;
+
+                if (ID > 0)
+                {
+                    openProc = m.OpenProcess(ID);
+                }
+                
+                if (openProc)
+                {
+                    float newCellCount = m.readFloat("0x20642D60"); // Reads current amount of collected cells.
+                    int newEcoType = m.readByte("0x20182848"); // Reads eco type.
+                    float oneHitKill = m.readFloat("0x20182860"); // Reads current big health.
+
+                    if (checkBox3.Checked == true) // If One Hit Kill checkbox is checked.
+                    {
+                        m.writeMemory("0x20182860", "float", health[0]); // Write value 1 to big health address.
+                        System.Threading.Thread.Sleep(2000); // Wait 2 seconds before writing value 1 to big health address again.
+                    }
+
+                    if (checkBox2.Checked == true && newEcoType > 0) // If Eco checkbox is checked and eco type address is 0.
+                    {
+                        Random ran = new Random();
+                        int num = ran.Next(4); // Generates random number up to 4.
+                        m.writeMemory("0x20182848", "byte", eco[num]); // Writes random value to the eco type address.
+                        System.Threading.Thread.Sleep(1000); // Waits 1 second to change eco type.
+                    }
+
+                    if (checkBox.Checked == true && checkBox1.Checked == true) // If both Cell randomizer checkboxes are checked...
+                    {
+                        ; // Do nothing.
+                    }
+
+                    else
+                    {
+                        if (checkBox.Checked == true && checkBox1.Checked == false && newCellCount > cellCount) // Checks if the cell count went up.
+                        {
+                            Random ran = new Random();
+                            int num = ran.Next(52); // Generates a random number up to x.
+                            System.Threading.Thread.Sleep(8000); // Waits for cell cutscene to finish.
+                            //m.writeMemory("0x2017A9F8", "float", "1328623"); // Changes Z coordinate to out of bounds.
+                            m.writeMemory("0x2017A9F4", "float", "-2127581"); // Changes Y coordinate to out of bounds.
+                            m.writeMemory("0x207CB984", "bytes", spawn[num]); // Uses randomly genertated number to determine spawn value.
+                            m.writeMemory("0x20642D6C", "bytes", checkpoint[num]); // Uses randomly genertated number to determine checkpoint value.
+                            cellCount = newCellCount; // Updates static cell count to number of cells the player has.
+                        }
+                    
+
+                        if (checkBox1.Checked == true && checkBox.Checked == false && newCellCount > cellCount + 2) // Checks if the cell count went up by 3.
+                        {
+                            Random ran = new Random();
+                            int num = ran.Next(52); // Generates a random number up to x.
+                            System.Threading.Thread.Sleep(8000); // Waits for cell cutscene to finish.
+                            // m.writeMemory("0x2017A9F8", "float", "1328623"); // Changes Z coordinate to out of bounds.
+                            m.writeMemory("0x2017A9F4", "float", "-2127581"); // Changes Y coordinate to out of bounds.
+                            m.writeMemory("0x207CB984", "bytes", spawn[num]); // Uses randomly genertated number to determine spawn value.
+                            m.writeMemory("0x20642D6C", "bytes", checkpoint[num]); // Uses randomly genertated number to determine checkpoint value.
+                            cellCount = newCellCount; // Updates static cell count to number of cells the player has.
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!backgroundWorker1.IsBusy)
+                backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://www.twitter.com/ruhphorte");
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://discord.gg/HUzXuNn");
+        }
+    }
+}
